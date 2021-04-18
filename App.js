bae9498091acf62fa3563c19140f770e6cab830f
Fidel Jr. Rillon <fideljr.rillon@benilde.edu.ph>
2021-04-18 21:25:40 +0800

    first commit. card has been installed under the markdown function

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View, Button } from 'react-native';
+import Icons from 'react-native-vector-icons/MaterialCommunityIcons';
+import CardScreen from './components/card';
 
 export default function App() {
+  const [showMarkdown, setShowMarkdown] = useState(false);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TouchableOpacity
+        onPress={() => setShowMarkdown(!showMarkdown)}
+      >
+      <Icons
+        reverse 
+        name="arrow-down-drop-circle-outline" 
+        size={23} 
+        color="#517fa4" />
+      </TouchableOpacity>
+      {showMarkdown == false ? null : (
+        <View>
+          <CardScreen>
+          <View style={styles.buttonStyle}>
+                    <Button color={'#F8CD81'} title='Reset' onPress={() => {}} />
+                    <Button color={'#D17B47'} title='Confirm' onPress={() => {}} />
+          </View>
+          </CardScreen>
+        </View>
+      )}
     </View>
   );
 }
@@ -14,8 +34,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    display: 'flex',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttonStyle: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingTop: 25
+},
 });
